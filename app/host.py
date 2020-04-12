6d1ce77805a944bb788bf068c968f63c7886e999
hake0703 <hake0703@gmail.com>
2020-04-12 16:57:44 -0500

    All is working

--- app/host.py
+++ app/host.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+import sensor
 
+app = Flask(__name__)
 host_external = "0.0.0.0" # Allows app to be externally available
+sense = sensor.SonicSensor()
+
 
 @app.route("/")
 def hello():
@@ -14,6 +17,12 @@ def open():
 @app.route("/close")
 def close():
     return "Garage Close... Bye, Bye."
+
+@app.route("/status")
+def status():
+    travel_time = sensor.get_signal_travel_time(sense)
+    dist = sensor.get_distance_inches(travel_time)
+    return ("Distance = %.3f inches" % dist)
 
 if __name__ == "__main__":
     app.run(host=host_external)
